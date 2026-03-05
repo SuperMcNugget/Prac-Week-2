acd44ffa67897f1b6a69e0bb888b381a8e6f1b06
Aiden Coorey <Aiden.coorey@gmail.com>
2026-03-05 17:26:10 +1000

    Added ToDoList Class linked it to the logged in user

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -82,6 +82,7 @@ public class Main {
         User user = authService.logIn(username, password);
         System.out.println("Welcome, " + user.getUsername() + "!");
         // TODO Later: Add the to-do list operations
+        ToDoList todoList = ToDoList(user);
     }
 
     /**
